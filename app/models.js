5a905b2a2b9a097fbee12defb8ac5c1a2c8265ce
Lauren Ancona <lauren.ancona@phila.gov>
2016-02-25 19:03:54 -0500

    merge conflicts

--- app/models.js
+++ app/models.js
@@ -1,6 +1,31 @@
-// Connect to the database.
-var db = {};
+var mongoose = require('mongoose');
+var Mixed = mongoose.Schema.Types.Mixed;
 
 module.exports = {
-    data: db
+    Analytics: mongoose.model(
+        'AnalyticsSchema', new mongoose.Schema({
+            name: {
+                type: String,
+                required: true,
+                unique: true
+            },
+             query: {
+                type: Mixed,
+                required: true,
+                unique: true
+            },
+            update_interval: {
+                type: Number,
+                required: false
+            },
+            last_update: {
+                type: Number,
+                required: false
+            },
+            data: {
+                type: Mixed,
+                required: false
+            }
+        })
+    )
 };
