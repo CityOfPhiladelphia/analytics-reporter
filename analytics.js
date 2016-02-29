636c1999540df60b7b49aeffc3e7856332a7faf0
Lauren Ancona <lauren.ancona@phila.gov>
2016-02-29 02:20:38 -0500

    comment custom dimensions

--- analytics.js
+++ analytics.js
@@ -135,7 +135,7 @@ var Analytics = {
         "ga:eventLabel": "event_label",
         "ga:eventAction": "event_action",
         "ga:totalEvents": "total_events",
-        "ga:dimension1": "full_url",
+        "ga:dimension1": "full_url", // custom dimension slot 1: unaltered request URI (Full URL)
         "rt:country": "country",
         "rt:city": "city",
         "rt:totalEvents": "total_events",
